792894a9e3195132011977a0feab5cd70bca8416
crisedulo <crisedulo@gmail.com>
2019-08-14 10:48:05 -0600

    Updates

---
+++ precache-manifest.2a0e03a5ca7066e9bed7bcc719dc99e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbac5cf491b1684e41c62425cdc1990",
+    "url": "/reto-reactjs/index.html"
+  },
+  {
+    "revision": "66f9a5d01b0d74b1a0b5",
+    "url": "/reto-reactjs/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3f22128351bbec6ee617",
+    "url": "/reto-reactjs/static/css/main.76d5ddda.chunk.css"
+  },
+  {
+    "revision": "66f9a5d01b0d74b1a0b5",
+    "url": "/reto-reactjs/static/js/2.16b6ecd0.chunk.js"
+  },
+  {
+    "revision": "3f22128351bbec6ee617",
+    "url": "/reto-reactjs/static/js/main.d3266cb9.chunk.js"
+  },
+  {
+    "revision": "ad301cd166251ab46f8b",
+    "url": "/reto-reactjs/static/js/runtime~main.84126e41.js"
+  }
+]);
